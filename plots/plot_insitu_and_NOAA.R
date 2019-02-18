edaa78a638765790d99cccdb3e44ff2e8b34f1e3
daniclaar <danielle.claar@gmail.com>
2019-02-18 15:02:54 -0800

    plot satellite and in situ data

--- plots/plot_insitu_and_NOAA.R
+++ plots/plot_insitu_and_NOAA.R
@@ -5,22 +5,47 @@ rm(list=ls())
 library(ggplot2)
 
 # Load necessary data
-load("data/KI_insitu_temperature.RData")
+load("data/KI_SB_temp_1d.RData")
 load("data/NOAA_CoralTemp_2011_2018.RData")
 
 plot_temps <- function(insitu,satellite){
-ggplot()+ theme_classic()+theme(legend.position = c(0.125, 0.85))+
-  geom_line(aes(x=xi2,y=temperature_1hr, color="In situ"),
+ggplot()+ theme_classic()+theme(legend.position = c(0.075, 0.87))+
+  geom_line(aes(x=xi3,y=temperature_1d, color="In situ"),
             data=insitu)+
   geom_line(aes(x=date,y=sst, color="Satellite"),data=satellite)+
-  scale_color_manual(name="Data Source", values = c("darkgray","turquoise"))+
-    xlab("Date")+
-    scale_y_continuous(name="Temperature (°C)")+
+  scale_color_manual(name=NULL, values = c("gray50","turquoise"))+
+  scale_x_datetime(name="Date", expand=c(0,0))+
+  scale_y_continuous(name="Temperature (°C)")+
   NULL
 }
 
-plot_temps(bayofwrecks_1hr_insitu,sst_BOW)
-plot_temps(northlagoon_1hr_insitu,sst_northlagoon)
-plot_temps(northshore_1hr_insitu,sst_northshore)
-plot_temps(vaskesbay_1hr_insitu,sst_vaskess)
-plot_temps(southlagoon_1hr_insitu,sst_southlagoon)
+plot_temps(bayofwrecks_1d,sst_BOW)
+plot_temps(northlagoon_1d,sst_northlagoon)
+plot_temps(northshore_1d,sst_northshore)
+plot_temps(vaskesbay_1d,sst_vaskess)
+plot_temps(southlagoon_1d,sst_southlagoon)
+
+BOW <- plot_temps(bayofwrecks_1d,sst_BOW)+
+  annotate("text",x=as.POSIXct("2018/1/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                               tz="Pacific/Kiritimati"),
+           y=30.5, label="Bay of Wrecks")
+NL <- plot_temps(northlagoon_1d,sst_northlagoon)+guides(color=FALSE)+
+  annotate("text",x=as.POSIXct("2018/1/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                               tz="Pacific/Kiritimati"),
+           y=30.5, label="North Lagoon")
+NS <- plot_temps(northshore_1d,sst_northshore)+guides(color=FALSE)+
+  annotate("text",x=as.POSIXct("2018/1/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                               tz="Pacific/Kiritimati"),
+           y=30.5, label="North Shore")
+VB <- plot_temps(vaskesbay_1d,sst_vaskess)+guides(color=FALSE)+
+  annotate("text",x=as.POSIXct("2018/1/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                               tz="Pacific/Kiritimati"),
+           y=30.5, label="Vaskess Bay")
+SL <- plot_temps(southlagoon_1d,sst_southlagoon)+guides(color=FALSE)+
+  annotate("text",x=as.POSIXct("2018/1/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
+                               tz="Pacific/Kiritimati"),
+           y=30.5, label="South Lagoon")
+
+pdf(file = "plots/KI_insitu_satellite_temps.pdf", width = 7.5, height = 10, useDingbats = FALSE)
+grid.arrange(VB,SL,NL,NS,BOW, nrow=5)
+dev.off()
