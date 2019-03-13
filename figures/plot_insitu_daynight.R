29dca727745d55d4b04c5a96cd44de00bbb3cc9c
daniclaar <danielle.claar@gmail.com>
2019-03-12 21:59:33 -0700

    update daynight calcs and plot to incl kims data

--- figures/plot_insitu_daynight.R
+++ figures/plot_insitu_daynight.R
@@ -4,9 +4,10 @@ rm(list=ls())
 # Load necessary packages
 library(ggplot2)
 library(gridExtra)
+library(scales)
 
 # Load necessary data
-load("data/KI_SB_temp_1d.RData")
+load("data/KI_SB_temp_wKim_1d.RData")
 
 region.cols<-c("VaskessBay" = "#5F4690","SouthLagoon"="#1D6996",
                "MidLagoon"="#0F8554","NorthLagoon"="#EDAD08",
@@ -15,24 +16,24 @@ region.cols<-c("VaskessBay" = "#5F4690","SouthLagoon"="#1D6996",
                "MidLagoon_night"="#001F00","NorthLagoon_night"="#874700",
                "NorthShore_night"="#2F0000","BayofWrecks_night"="#330000")
 
-NonNAindex <- which(!is.na(vaskesbay_1d$temperature_1d))
+NonNAindex <- which(!is.na(vaskesbay_1d_wKim$temperature_1d))
 firstNonNA <- min(NonNAindex)
 firstNonNA
 lastNonNA <- max(NonNAindex)
 lastNonNA
 
-bayofwrecks_1d <- bayofwrecks_1d[c(233:nrow(bayofwrecks_1d)),]
-bayofwrecks_night_1d <- bayofwrecks_night_1d[c(233:nrow(bayofwrecks_night_1d)),]
-northlagoon_1d <- northlagoon_1d[c(233:nrow(northlagoon_1d)),]
-northlagoon_night_1d <- northlagoon_night_1d[c(233:nrow(northlagoon_night_1d)),]
-northshore_1d <- northshore_1d[c(233:nrow(northshore_1d)),]
-northshore_night_1d <- northshore_night_1d[c(233:nrow(northshore_night_1d)),]
-vaskesbay_1d <- vaskesbay_1d[c(233:nrow(vaskesbay_1d)),]
-vaskesbay_night_1d <- vaskesbay_night_1d[c(233:nrow(vaskesbay_night_1d)),]
-southlagoon_1d <- southlagoon_1d[c(233:nrow(southlagoon_1d)),]
-southlagoon_night_1d <- southlagoon_night_1d[c(233:nrow(southlagoon_night_1d)),]
-lagoonface_1d <- lagoonface_1d[c(233:nrow(lagoonface_1d)),]
-lagoonface_night_1d <- lagoonface_night_1d[c(233:nrow(lagoonface_night_1d)),]
+bayofwrecks_1d_wKim <- bayofwrecks_1d_wKim[c(233:nrow(bayofwrecks_1d_wKim)),]
+bayofwrecks_night_wKim_1d <- bayofwrecks_night_wKim_1d[c(233:nrow(bayofwrecks_night_wKim_1d)),]
+northlagoon_1d_wKim <- northlagoon_1d_wKim[c(233:nrow(northlagoon_1d_wKim)),]
+northlagoon_night_wKim_1d <- northlagoon_night_wKim_1d[c(233:nrow(northlagoon_night_wKim_1d)),]
+northshore_1d_wKim <- northshore_1d_wKim[c(233:nrow(northshore_1d_wKim)),]
+northshore_night_wKim_1d <- northshore_night_wKim_1d[c(233:nrow(northshore_night_wKim_1d)),]
+vaskesbay_1d_wKim <- vaskesbay_1d_wKim[c(233:nrow(vaskesbay_1d_wKim)),]
+vaskesbay_night_wKim_1d <- vaskesbay_night_wKim_1d[c(233:nrow(vaskesbay_night_wKim_1d)),]
+southlagoon_1d_wKim <- southlagoon_1d_wKim[c(233:nrow(southlagoon_1d_wKim)),]
+southlagoon_night_wKim_1d <- southlagoon_night_wKim_1d[c(233:nrow(southlagoon_night_wKim_1d)),]
+lagoonface_1d_wKim <- lagoonface_1d_wKim[c(233:nrow(lagoonface_1d_wKim)),]
+lagoonface_night_wKim_1d <- lagoonface_night_wKim_1d[c(233:nrow(lagoonface_night_wKim_1d)),]
 
 
 plot_temps_daynight <- function(day,night){
@@ -42,42 +43,42 @@ plot_temps_daynight <- function(day,night){
     geom_line(aes(x=xi3,y=temperature_1d, color="Night-only"),data=night,alpha=0.5)+
     scale_color_manual(name=NULL, values = c("gray70","black"))+
     scale_x_datetime(name="Date", expand=c(0,0))+
-    scale_y_continuous(name="Temperature (°C)",limits = c(24,31))+
+    scale_y_continuous(name="Temperature (?C)",limits = c(24,31))+
     NULL
 }
 
-plot_temps_daynight(bayofwrecks_1d,bayofwrecks_night_1d)
-plot_temps_daynight(northlagoon_1d,northlagoon_night_1d)
-plot_temps_daynight(northshore_1d,northshore_night_1d)
-plot_temps_daynight(vaskesbay_1d,vaskesbay_night_1d)
-plot_temps_daynight(southlagoon_1d,southlagoon_night_1d)
-plot_temps_daynight(lagoonface_1d,lagoonface_night_1d)
+plot_temps_daynight(bayofwrecks_1d_wKim,bayofwrecks_night_wKim_1d)
+plot_temps_daynight(northlagoon_1d_wKim,northlagoon_night_wKim_1d)
+plot_temps_daynight(northshore_1d_wKim,northshore_night_wKim_1d)
+plot_temps_daynight(vaskesbay_1d_wKim,vaskesbay_night_wKim_1d)
+plot_temps_daynight(southlagoon_1d_wKim,southlagoon_night_wKim_1d)
+plot_temps_daynight(lagoonface_1d_wKim,lagoonface_night_wKim_1d)
 
-BOW <- plot_temps_daynight(bayofwrecks_1d,bayofwrecks_night_1d)+
+BOW <- plot_temps_daynight(bayofwrecks_1d_wKim,bayofwrecks_night_wKim_1d)+
   guides(color=FALSE)+
   annotate("text",x=as.POSIXct("2016/8/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="Bay of Wrecks")
-NL <- plot_temps_daynight(northlagoon_1d,northlagoon_night_1d)+
+NL <- plot_temps_daynight(northlagoon_1d_wKim,northlagoon_night_wKim_1d)+
   guides(color=FALSE)+
   annotate("text",x=as.POSIXct("2016/8/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="North Lagoon")
-NS <- plot_temps_daynight(northshore_1d,northshore_night_1d)+
+NS <- plot_temps_daynight(northshore_1d_wKim,northshore_night_wKim_1d)+
   guides(color=FALSE)+
   annotate("text",x=as.POSIXct("2016/8/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="North Shore")
-VB <- plot_temps_daynight(vaskesbay_1d,vaskesbay_night_1d)+
+VB <- plot_temps_daynight(vaskesbay_1d_wKim,vaskesbay_night_wKim_1d)+
   annotate("text",x=as.POSIXct("2016/8/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="Vaskess Bay")
-SL <- plot_temps_daynight(southlagoon_1d,southlagoon_night_1d)+
+SL <- plot_temps_daynight(southlagoon_1d_wKim,southlagoon_night_wKim_1d)+
   guides(color=FALSE)+
   annotate("text",x=as.POSIXct("2016/8/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="South Lagoon")
-LF <- plot_temps_daynight(lagoonface_1d,lagoonface_night_1d)+
+LF <- plot_temps_daynight(lagoonface_1d_wKim,lagoonface_night_wKim_1d)+
   guides(color=FALSE)+
   annotate("text",x=as.POSIXct("2016/8/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
@@ -90,22 +91,22 @@ dev.off()
 
 ####
 
-bayofwrecks_1d_sub <- bayofwrecks_1d[c(1360:1420),]
-bayofwrecks_night_1d_sub <- bayofwrecks_night_1d[c(1360:1420),]
-northlagoon_1d_sub <- northlagoon_1d[c(1360:1420),]
-northlagoon_night_1d_sub <- northlagoon_night_1d[c(1360:1420),]
-northshore_1d_sub <- northshore_1d[c(1360:1420),]
-northshore_night_1d_sub <- northshore_night_1d[c(1360:1420),]
-vaskesbay_1d_sub <- vaskesbay_1d[c(1360:1420),]
-vaskesbay_night_1d_sub <- vaskesbay_night_1d[c(1360:1420),]
-southlagoon_1d_sub <- southlagoon_1d[c(1360:1420),]
-southlagoon_night_1d_sub <- southlagoon_night_1d[c(1360:1420),]
-lagoonface_1d_sub <- lagoonface_1d[c(1360:1420),]
-lagoonface_night_1d_sub <- lagoonface_night_1d[c(1360:1420),]
+bayofwrecks_1d_wKim_sub <- bayofwrecks_1d_wKim[c(1360:1420),]
+bayofwrecks_night_wKim_1d_sub <- bayofwrecks_night_wKim_1d[c(1360:1420),]
+northlagoon_1d_wKim_sub <- northlagoon_1d_wKim[c(1360:1420),]
+northlagoon_night_wKim_1d_sub <- northlagoon_night_wKim_1d[c(1360:1420),]
+northshore_1d_wKim_sub <- northshore_1d_wKim[c(1360:1420),]
+northshore_night_wKim_1d_sub <- northshore_night_wKim_1d[c(1360:1420),]
+vaskesbay_1d_wKim_sub <- vaskesbay_1d_wKim[c(1360:1420),]
+vaskesbay_night_wKim_1d_sub <- vaskesbay_night_wKim_1d[c(1360:1420),]
+southlagoon_1d_wKim_sub <- southlagoon_1d_wKim[c(1360:1420),]
+southlagoon_night_wKim_1d_sub <- southlagoon_night_wKim_1d[c(1360:1420),]
+lagoonface_1d_wKim_sub <- lagoonface_1d_wKim[c(1360:1420),]
+lagoonface_night_wKim_1d_sub <- lagoonface_night_wKim_1d[c(1360:1420),]
 
-BOW_sub <- plot_temps_daynight(bayofwrecks_1d_sub,bayofwrecks_night_1d_sub)+
+BOW_sub <- plot_temps_daynight(bayofwrecks_1d_wKim_sub,bayofwrecks_night_wKim_1d_sub)+
   # guides(color=FALSE)+    
-  scale_y_continuous(name="Temperature (°C)",limits = c(28.3,30),expand=c(0,0))+
+  scale_y_continuous(name="Temperature (?C)",limits = c(28.3,30),expand=c(0,0))+
   annotate("text",x=as.POSIXct("2015/7/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="Bay of Wrecks")+
@@ -113,9 +114,9 @@ BOW_sub <- plot_temps_daynight(bayofwrecks_1d_sub,bayofwrecks_night_1d_sub)+
   scale_x_datetime(name="Date", expand=c(0,0),breaks=date_breaks("1 month"),
                    labels=date_format('%b %Y'))
 
-NL_sub <- plot_temps_daynight(northlagoon_1d_sub,northlagoon_night_1d_sub)+
+NL_sub <- plot_temps_daynight(northlagoon_1d_wKim_sub,northlagoon_night_wKim_1d_sub)+
   # guides(color=FALSE)+
-  scale_y_continuous(name="Temperature (°C)",limits = c(28.3,30),expand=c(0,0))+
+  scale_y_continuous(name="Temperature (?C)",limits = c(28.3,30),expand=c(0,0))+
   annotate("text",x=as.POSIXct("2015/7/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="North Lagoon")+
@@ -124,9 +125,9 @@ NL_sub <- plot_temps_daynight(northlagoon_1d_sub,northlagoon_night_1d_sub)+
                    labels=date_format('%b %Y'))
 
 
-NS_sub <- plot_temps_daynight(northshore_1d_sub,northshore_night_1d_sub)+
+NS_sub <- plot_temps_daynight(northshore_1d_wKim_sub,northshore_night_wKim_1d_sub)+
   # guides(color=FALSE)+
-  scale_y_continuous(name="Temperature (°C)",limits = c(28.3,30),expand=c(0,0))+
+  scale_y_continuous(name="Temperature (?C)",limits = c(28.3,30),expand=c(0,0))+
   annotate("text",x=as.POSIXct("2015/7/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="North Shore")+
@@ -135,20 +136,20 @@ NS_sub <- plot_temps_daynight(northshore_1d_sub,northshore_night_1d_sub)+
                    labels=date_format('%b %Y'))
 
 
-VB_sub <- plot_temps_daynight(vaskesbay_1d_sub,vaskesbay_night_1d_sub)+
+VB_sub <- plot_temps_daynight(vaskesbay_1d_wKim_sub,vaskesbay_night_wKim_1d_sub)+
   annotate("text",x=as.POSIXct("2015/7/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="Vaskess Bay")+
-  scale_y_continuous(name="Temperature (°C)",limits = c(28.3,30),expand=c(0,0))+
+  scale_y_continuous(name="Temperature (?C)",limits = c(28.3,30),expand=c(0,0))+
   scale_color_manual(name=NULL, values = c(region.cols[["VaskessBay"]],region.cols[["VaskessBay_night"]]))+
   scale_x_datetime(name="Date", expand=c(0,0),breaks=date_breaks("1 month"),
                    labels=date_format('%b %Y'))
 
 
   
-SL_sub <- plot_temps_daynight(southlagoon_1d_sub,southlagoon_night_1d_sub)+
+SL_sub <- plot_temps_daynight(southlagoon_1d_wKim_sub,southlagoon_night_wKim_1d_sub)+
   # guides(color=FALSE)+
-  scale_y_continuous(name="Temperature (°C)",limits = c(28.3,30),expand=c(0,0))+
+  scale_y_continuous(name="Temperature (?C)",limits = c(28.3,30),expand=c(0,0))+
 annotate("text",x=as.POSIXct("2015/7/1 00:00:00",format="%Y/%m/%d %H:%M:%S",
                                tz="Pacific/Kiritimati"),
            y=30.5, label="South Lagoon")+
@@ -166,3 +167,4 @@ dev.off()
 jpeg(filename = "figures/KI_insitu_daynight_temps_subset.jpg", width = 7.5, height = 10, units="in",res = 300)
 grid.arrange(VB_sub,SL_sub,NL_sub,NS_sub,BOW_sub, nrow=5)
 dev.off()
+
